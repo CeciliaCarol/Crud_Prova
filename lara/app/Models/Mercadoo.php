362258c05f34f8ee9953a7ca0deff150bcb4dbbe
Cecília Carolina <109114160+CeciliaCarol@users.noreply.github.com>
2023-05-17 10:23:50 -0300

    Update and rename Projeto.php to Mercadoo.php

--- lara/app/Models/Mercadoo.php
+++ lara/app/Models/Mercadoo.php
@@ -5,16 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Projeto extends Model
+class Mercado extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'nome',
-        'descricao',
-        'inicio',
-        'termino',
-        'responsavel',
+        'endereco',
+        'telefone',
+        'ncaixas',
+        'hfunciona',
         'user_id'
     ];
 }
