d5059470f2090da1195e53c1391dc9cf87edff72
Cecília Carolina <109114160+CeciliaCarol@users.noreply.github.com>
2023-05-17 10:19:51 -0300

    Update web.php

--- lara/routes/web.php
+++ lara/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProjetoController;
+use App\Http\Controllers\MercadoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('/projeto', ProjetoController::class)->except(['dashboard']);
+    Route::resource('/mercado', MercadoController::class)->except(['dashboard']);
 });
 
 require __DIR__.'/auth.php';
